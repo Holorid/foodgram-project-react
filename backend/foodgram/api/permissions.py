5e972228891fc325bd91a615b4ca83c95a695e14
Денис Бобенко <bbenko01@yandex.ru>
2023-07-29 17:11:49 +0300

    pip8 permissions

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -5,8 +5,8 @@ class IsAuthotOrAuthenticatedOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.method in permissions.SAFE_METHODS
-            or request.user and
-            request.user.is_authenticated
+            or request.user
+            and request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
